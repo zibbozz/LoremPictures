4ea43859a3c84e397a50c8ef86c8017e7d8c9435
Sebastian Wittmann <sebastian.wittmann97@web.de>
2019-11-28 16:40:57 +0100

    added picture service

---
+++ index.js
@@ -0,0 +1,164 @@
+let http = require("http");
+let express = require("express");
+let app = express();
+let fs = require("fs");
+let sharp = require("sharp");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(__dirname + '/app'));
+
+let server = http.createServer(app);
+server.listen(8080, () => {
+	console.log("Server listening on Port 8080");
+});
+
+app.get('/id/:id', function(req, res){
+	try {
+		res.contentType("image/jpeg");
+		let rs = fs.createReadStream(__dirname + '/images/' + req.params.id + '.jpg');
+		rs.on('open', function(){
+			rs.pipe(res);
+		});
+		rs.on('error', function(){
+			res.contentType("text/plain");
+			res.status(200).send("Bild mit dieser ID existiert nicht.");
+		});
+	} catch {
+		res.contentType("text/plain");
+		res.status(200).send("Bild mit dieser ID existiert nicht.");
+	}
+});
+
+app.get('/random', function(req, res){
+	let index = Math.floor(Math.random() * Math.floor(992));
+	res.contentType("image/jpeg");
+	let rs = fs.createReadStream(__dirname + '/images/' + index + '.jpg');
+	rs.on('open', function(){
+		rs.pipe(res);
+	});
+	rs.on('error', function(){
+		res.contentType("text/plain");
+		res.status(200).send("Bild mit dieser ID existiert nicht.");
+	});
+});
+
+app.get('/id/:id/:size', function(req, res){
+	try {
+		res.contentType("image/jpeg");
+		let image = sharp(__dirname + '/images/' + req.params.id + '.jpg');
+		image.resize(parseInt(req.params.size), parseInt(req.params.size));
+		if(req.query.grayscale != null || req.query.greyscale != null){
+			image.toColorspace('b-w');
+		}
+		if(req.query.blur != null){
+
+			let blurValue = req.query.blur != "" ? parseInt(req.query.blur) : 5;
+			if(blurValue < 1){
+				blurValue = 1;
+			}
+			if(blurValue > 100){
+				blurValue = 100;
+			}
+			image.blur(blurValue);
+		}
+		image.toBuffer().then(data => {
+			res.status(200).send(data);	
+		})
+	} catch(e) {
+		res.contentType("text/plain");
+		res.status(200).send("Bild mit dieser ID existiert nicht.");
+		console.log(e);
+	}
+});
+
+app.get('/random/:size', function(req, res){
+	let index = Math.floor(Math.random() * Math.floor(992));
+	try {
+		res.contentType("image/jpeg");
+		let image = sharp(__dirname + '/images/' + index + '.jpg');
+		image.resize(parseInt(req.params.size), parseInt(req.params.size));
+		if(req.query.grayscale != null || req.query.greyscale != null){
+			image.toColorspace('b-w');
+		}
+		if(req.query.blur != null){
+
+			let blurValue = req.query.blur != "" ? parseInt(req.query.blur) : 5;
+			if(blurValue < 1){
+				blurValue = 1;
+			}
+			if(blurValue > 100){
+				blurValue = 100;
+			}
+			image.blur(blurValue);
+		}
+		image.toBuffer().then(data => {
+			res.status(200).send(data);	
+		})
+	} catch(e) {
+		res.contentType("text/plain");
+		res.status(200).send("Bild mit dieser ID existiert nicht.");
+		console.log(e);
+	}
+});
+
+app.get('/id/:id/:sizex/:sizey', function(req, res){
+	try {
+		res.contentType("image/jpeg");
+		let image = sharp(__dirname + '/images/' + req.params.id + '.jpg');
+		image.resize(parseInt(req.params.sizex), parseInt(req.params.sizey));
+		if(req.query.grayscale != null || req.query.greyscale != null){
+			image.toColorspace('b-w');
+		}
+		if(req.query.blur != null){
+
+			let blurValue = req.query.blur != "" ? parseInt(req.query.blur) : 5;
+			if(blurValue < 1){
+				blurValue = 1;
+			}
+			if(blurValue > 100){
+				blurValue = 100;
+			}
+			image.blur(blurValue);
+		}
+		image.toBuffer().then(data => {
+			res.status(200).send(data);	
+		})
+		
+	} catch(e) {
+		res.contentType("text/plain");
+		res.status(200).send("Bild mit dieser ID existiert nicht.");
+		console.log(e);
+	}	
+});
+
+app.get('/random/:sizex/:sizey', function(req, res){
+	let index = Math.floor(Math.random() * Math.floor(992));
+	try {
+		res.contentType("image/jpeg");
+		let image = sharp(__dirname + '/images/' + index + '.jpg');
+		image.resize(parseInt(req.params.sizex), parseInt(req.params.sizey));
+		if(req.query.grayscale != null || req.query.greyscale != null){
+			image.toColorspace('b-w');
+		}
+		if(req.query.blur != null){
+
+			let blurValue = req.query.blur != "" ? parseInt(req.query.blur) : 5;
+			if(blurValue < 1){
+				blurValue = 1;
+			}
+			if(blurValue > 100){
+				blurValue = 100;
+			}
+			image.blur(blurValue);
+		}
+		image.toBuffer().then(data => {
+			res.status(200).send(data);	
+		})
+		
+	} catch(e) {
+		res.contentType("text/plain");
+		res.status(200).send("Bild mit dieser ID existiert nicht.");
+		console.log(e);
+	}
+});
